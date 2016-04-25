faeaf9c623ca1bf6bc1cfcac3f792e835297ae41
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-04-25 15:51:49 -0300

    optionally profile the setup/teardown

--- lib/minitest/stackprof_plugin.rb
+++ lib/minitest/stackprof_plugin.rb
@@ -1,13 +1,41 @@
 module Minitest
     module StackProfPlugin
+        @include_setup_and_teardown = false
+
+        def self.include_setup_and_teardown?
+            @include_setup_and_teardown
+        end
+
+        def self.include_setup_and_teardown=(flag)
+            @include_setup_and_teardown = !!flag
+        end
+
         def before_setup
-            StackProf.start
+            if StackProfPlugin.include_setup_and_teardown?
+                StackProf.start(raw: true)
+            end
             super
+        end
+
+        def after_setup
+            super
+            if !StackProfPlugin.include_setup_and_teardown?
+                StackProf.start(raw: true)
+            end
+        end
+
+        def before_teardown
+            super
+            if !StackProfPlugin.include_setup_and_teardown?
+                StackProf.stop
+            end
         end
 
         def after_teardown
             super
-            StackProf.stop
+            if StackProfPlugin.include_setup_and_teardown?
+                StackProf.stop
+            end
         end
     end
 
@@ -24,14 +52,18 @@ module Minitest
     end
 
     def self.plugin_stackprof_options(opts, options)
-        opts.on '--stackprof[=FILE]', 'run under stackprof (optionally specifies the mode)' do |mode|
-            options[:stackprof] = mode || "stackprof-minitest-#{Process.pid}.dump"
+        opts.on '--profile-setup', 'profile the setup and teardown as well as the test' do
+            StackProfPlugin.include_setup_and_teardown = true
+        end
+        opts.on '--stackprof[=FILE]', String, 'run under stackprof (optionally specifies the output file)' do |file|
+            options[:stackprof] = file || "stackprof-minitest-#{Process.pid}.dump"
         end
     end
     def self.plugin_stackprof_init(options)
         if output_file = options[:stackprof]
             require 'stackprof'
             puts "Stackprof enabled - dumping results to #{output_file}"
+            puts "Stackprof: #{"NOT " if !StackProfPlugin.include_setup_and_teardown?} profiling the setup/teardown"
             Minitest::Test.prepend StackProfPlugin
             reporter << StackProfReporter.new(output_file)
         end
